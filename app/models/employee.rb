0766468ebf52400105f2b8c6ba9ae27767034ba7
srinivas <srinivas08478@gmail.com>
2016-09-19 17:02:33 +0530

    remove validations

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -6,8 +6,8 @@ class Employee < ActiveRecord::Base
   belongs_to :role
   has_many :subordinates, class_name: "Employee", foreign_key: "manager_id" 
   belongs_to :manager, class_name: "Employee"
-  validates_presence_of :first_name, :last_name, :role_id, :department_id, :ttid, :personal_email, :contact_no, :emergency_name, :emergency_contact_no, :actual_dob, :certificate_dob, :doj 
-  validates_presence_of :graduation, :source_of_hire, :pancard_no, :passport_no, :status, :address
+  #validates_presence_of :first_name, :last_name, :role_id, :department_id, :ttid, :personal_email, :contact_no, :emergency_name, :emergency_contact_no, :actual_dob, :certificate_dob, :doj 
+  #validates_presence_of :graduation, :source_of_hire, :pancard_no, :passport_no, :status, :address
   belongs_to :department
 
   def fullname
