2c778e58b0e5f02905474406af58067230ae10f5
srinivas <srinivas08478@gmail.com>
2016-09-30 10:09:27 +0530

    added email configuation in production mode

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'http://tthrm.herokuapp.com' }
+
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :domain               => "gmail.com",
+      :user_name            => "srinivas08478@gmail.com",
+      :password             => "sriniv@s143",
+      :authentication       => :plain,
+      :enable_starttls_auto => true
+  }
 end
