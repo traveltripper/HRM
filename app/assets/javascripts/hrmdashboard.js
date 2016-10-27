372b83a7d020d4f649e95910e7222f79d3943093
srinivas <srinivas08478@gmail.com>
2016-10-27 19:09:15 +0530

    added profile and team pages

--- app/assets/javascripts/hrmdashboard.js
+++ app/assets/javascripts/hrmdashboard.js
@@ -2,4 +2,14 @@ $(document).ready(function(){
 $(".emp_link").click(function(){
     $('#employee_details').show();
 });
+
+var selector = '.teammembers_img';
+
+$(selector).on('click', function(){
+    $(selector).removeClass('active');
+    $(this).addClass('active');
+});
+
+$(".team_list li:first").addClass("active");
+
 });
