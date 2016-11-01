95f74a7929d1f1040fa084015c3070c26edfc3bb
srinivas <srinivas08478@gmail.com>
2016-11-01 15:11:16 +0530

    added events

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,8 @@ class ApplicationController < ActionController::Base
   end
   
   def upcoming_events
-    Event.where('start >= ? or end >= ?', Date.today, Date.today).limit(5)
+    Event.all
+    #Event.where('start >= ? or end >= ?', Date.today, Date.today).limit(5)
   end
  
   rescue_from CanCan::AccessDenied do |exception|
