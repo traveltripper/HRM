ca3e33ad5ff9a04203edfc983483539b9d7620a8
srinivas <srinivas08478@gmail.com>
2016-11-02 12:39:16 +0530

    renamed event coloumn

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,7 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_employee!
+  load_and_authorize_resource
   layout 'dashboard'
   add_breadcrumb "Home", :root_path
   # GET /events
