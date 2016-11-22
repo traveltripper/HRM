2f737823f0e78c492268abfdf6f63ba8f84ca086
srinivas <srinivas08478@gmail.com>
2016-11-22 12:55:41 +0530

    added friendly id for events and announcements

--- app/models/event.rb
+++ app/models/event.rb
@@ -8,4 +8,8 @@ class Event < ActiveRecord::Base
  	belongs_to :employee
     has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/missing.jpg"
   	validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
+
+  	def should_generate_new_friendly_id?
+  		title_changed?
+	end
 end
