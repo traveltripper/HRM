39563fd277ec9f29a6ba5d0774634ed909a364c0
srinivas <srinivas08478@gmail.com>
2016-11-28 11:22:51 +0530

    added announcements in team page

--- app/assets/javascripts/alerts.js
+++ app/assets/javascripts/alerts.js
@@ -1,50 +1,50 @@
-$(function(){ // DOM ready
+// $(function(){ // DOM ready
 
-  // ::: TAGS BOX
-  var list = [];
-  $("#enter_emails").on({
+//   // ::: TAGS BOX
+//   var list = [];
+//   $("#enter_emails").on({
 
-    focusout : function() {
-      var txt = this.value.replace(/[^a-z0-9\+\-\@\.\#]/ig,''); // allowed characters   
+//     focusout : function() {
+//       var txt = this.value.replace(/[^a-z0-9\+\-\@\.\#]/ig,''); // allowed characters   
 
-          if(txt) 
-          {           
-              if(validateEmail(txt)){
-                $('.email-errors').hide();
-                $("<span/>", {text:txt, insertBefore:this});
-                this.value = "";
-                list.push(txt);
-                $('textarea#get_email').val(list);
-              }
-              else{
-                  this.value = "";
-                  $('.email-errors').show();
-              };                  
+//           if(txt) 
+//           {           
+//               if(validateEmail(txt)){
+//                 $('.email-errors').hide();
+//                 $("<span/>", {text:txt, insertBefore:this});
+//                 this.value = "";
+//                 list.push(txt);
+//                 $('textarea#get_email').val(list);
+//               }
+//               else{
+//                   this.value = "";
+//                   $('.email-errors').show();
+//               };                  
             
-          };                
-    },
+//           };                
+//     },
 
-    keyup : function(ev) {
-      // if: comma|enter (delimit more keyCodes with | pipe)
-      if(/(188|13)/.test(ev.which)) $(this).focusout();     
+//     keyup : function(ev) {
+//       // if: comma|enter (delimit more keyCodes with | pipe)
+//       if(/(188|13)/.test(ev.which)) $(this).focusout();     
 
-    }
-  });
+//     }
+//   });
 
-  $('#emails_list').on('click', 'span', function() {
-    //if(confirm("Remove "+ $(this).text() +"?")) 
-      $(this).remove();
-      var removeItem = $(this).text();
-      list = jQuery.grep(list, function(value) {
-        return value != removeItem;
-      }); 
-      $('textarea#get_email').val(list); 
-  });
+//   $('#emails_list').on('click', 'span', function() {
+//     //if(confirm("Remove "+ $(this).text() +"?")) 
+//       $(this).remove();
+//       var removeItem = $(this).text();
+//       list = jQuery.grep(list, function(value) {
+//         return value != removeItem;
+//       }); 
+//       $('textarea#get_email').val(list); 
+//   });
 
 
-  function validateEmail($email) {
-  var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;  
-  return emailReg.test( $email );
-  };
+//   function validateEmail($email) {
+//   var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;  
+//   return emailReg.test( $email );
+//   };
       
-});
+// });
