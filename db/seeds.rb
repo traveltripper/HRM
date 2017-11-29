d0a6751e6833ff99e9519a1e5ad3ac50fcc4643d
manasa-ttweb <metta.reddy@traveltripper.com>
2017-11-29 15:27:35 +0530

    commented data in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,18 +7,18 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-Role.create(name: "Employee")
-Role.create(name: "Manager")
-Role.create(name: "HR")
-Role.create(name: "Finace")
-Role.create(name: "Admin")
+# Role.create(name: "Employee")
+# Role.create(name: "Manager")
+# Role.create(name: "HR")
+# Role.create(name: "Finace")
+# Role.create(name: "Admin")
 
-Department.create(name: "Admin")
+# Department.create(name: "Admin")
 
-Leavetype.create(name: "Sick")
-Leavetype.create(name: "Maternity")
-Leavetype.create(name: "Paternity")
-Leavetype.create(name: "Vacation")
+# Leavetype.create(name: "Sick")
+# Leavetype.create(name: "Maternity")
+# Leavetype.create(name: "Paternity")
+# Leavetype.create(name: "Vacation")
 
 
 Employee.create!(first_name: "admin", middle_name: "", last_name: "traveltripper", role_id: 5, department_id: 1, ttid: "ttadmin1", email: "traveltripperhrm@traveltripper.com", personal_email: "admin@tt.com", contact_no: "123456789", emergency_name: "test", emergency_contact_no: "123456789", designation: "Admin", actual_dob: DateTime.strptime('08/26/1990', '%m/%d/%Y'), certificate_dob: DateTime.strptime('08/26/1990', '%m/%d/%Y'), prev_years_of_exp: 5, pg: "test", graduation: "test", source_of_hire: "Direct", pancard_no: "12356", passport_no: "123456", status: "Active", date_of_joining: DateTime.strptime('09/14/2016', '%m/%d/%Y'), address: "test", password: "123456", password_confirmation: "123456")
