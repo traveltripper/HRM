ee6e1ff34f64857bba036bcc182b27b96d32244b
manasa-ttweb <metta.reddy@traveltripper.com>
2017-12-19 13:28:03 +0530

    changes in controller

--- db/migrate/20160901072418_create_leavetypes.rb
+++ db/migrate/20160901072418_create_leavetypes.rb
@@ -2,7 +2,7 @@ class CreateLeavetypes < ActiveRecord::Migration
   def change
     create_table :leavetypes do |t|
       t.string :name
-      t.integer :limit
+      t.integer :limit, :default => 0
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161221101152) do
+ActiveRecord::Schema.define(version: 20171212061311) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace",     limit: 255
@@ -196,7 +196,7 @@ ActiveRecord::Schema.define(version: 20161221101152) do
 
   create_table "leavetypes", force: :cascade do |t|
     t.string   "name",       limit: 255
-    t.integer  "limit"
+    t.integer  "limit",      limit: 4
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
